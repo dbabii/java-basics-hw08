1aa2a1eb8c5fd56a8aecc73f73b62c86247c85e0
Dmytro Babii <dm.babii@gmail.com>
2022-05-31 22:26:54 +0300

    added body

--- src/RegExp.java
+++ src/RegExp.java
@@ -13,7 +13,6 @@ public class RegExp {
      * @return String with IP address
      */
     public static String getIpAddress(String text) {
-        // TODO fill in code here for extracting IP address using Regular Expression
         String firstOct = "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[1-9])";
         String otherOct = "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])";
         String regex = firstOct + "\\." + otherOct
